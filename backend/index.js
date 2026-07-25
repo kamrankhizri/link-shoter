53b608816cc052e39dd370b50a1cd2f6dd8dc4f3
Kamrankhizri <kamrankhizr4749@gmail.com>
2026-07-25 18:03:12 +0500

    Updated project

--- backend/index.js
+++ backend/index.js
@@ -21,20 +21,57 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("✅ Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection error:", err.message);
-  });
+// // MongoDB Connection
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => {
+//     console.log("✅ Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.error("❌ MongoDB connection error:", err.message);
+//   });
+    
+
+
+
+
+
+
+let isConnected = false;
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+
+
+
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectToMongoDB();
+  }
+  next();
+});
+
+
+
 
 // Routes
 app.use("/items", ProductsRoutes);
 
 // Start Server
-app.listen(port, () => {
-  console.log(`✅ Server is running on port ${port} 🚀`);
-});
+// app.listen(port, () => {
+//   console.log(`✅ Server is running on port ${port} 🚀`);
+// });
+
+module.exports =app
